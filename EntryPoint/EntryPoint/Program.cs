fef5f618ea9858899c64a3ab29001a4cf1481d50
mad kalna <madara.k@hotmail.co.uk>
2018-07-31 15:13:11 +0100

    try agian!

---
+++ EntryPoint/EntryPoint/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+class EntryPoint
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("http://testing.todorvachev.com/selectors/name/");
+        IWebElement element = driver.FindElement(By.Name("myName"));
+        if (element.Displayed)
+        {
+            GreenMessage("Pass!!!");
+        }
+        else
+        {
+            RedMessage("Something went Wrong!");
+        }
+        driver.Quit();
+     }
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
