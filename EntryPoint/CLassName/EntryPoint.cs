9f08dc24838dad311932fd54a703aedf03ca5654
mad kalna <madara.k@hotmail.co.uk>
2018-07-31 15:54:58 +0100

    Find Element by a class - showing in console that element has been read

---
+++ EntryPoint/CLassName/EntryPoint.cs
@@ -0,0 +1,43 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class EntryPoint
+    {
+        static void Main()
+        {
+        string url = "http://testing.todvachev.com/selectors/class-name/";
+        string className = "testClass";
+
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl(url);
+
+        IWebElement element = driver.FindElement(By.ClassName(className));
+
+        /* Grab the test element by class name and display it in the console
+         */
+
+        Console.WriteLine(element.Text);
+
+        
+        driver.Quit();
+        Thread.Sleep(4000);
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
